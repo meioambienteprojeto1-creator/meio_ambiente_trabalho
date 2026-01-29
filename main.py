154363fdc2d2b42651e2b4d64291aa9a7329b0f9
brenno <brennosaraiva2008@gmail.com>
2026-01-28 21:15:53 -0300

    update 1.12
    Refatore templates HTML para melhor estrutura e legibilidade; adicione chave de conta de serviço para integração com Firebase.

--- main.py
+++ main.py
@@ -291,10 +291,13 @@ def criar_campanha():
 @app.route("/descricao/<int:id>")
 def descricao(id):
     campanhas = get_campanhas()
-    if 0 <= id < len(campanhas):
-        campanha_selecionada = campanhas[id]
-    else:
-        campanha_selecionada = None
+    
+    # Procurar a campanha com o ID correspondente
+    campanha_selecionada = None
+    for campanha in campanhas:
+        if campanha.get("id") == id:
+            campanha_selecionada = campanha
+            break
 
     return render_template("descricao.html", campanha=campanha_selecionada)
 
